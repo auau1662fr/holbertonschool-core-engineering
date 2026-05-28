fd46128a5137317df471871cc1ca7bf0ee0552c4
auau1662fr <auric.chen.2005@gmail.com>
2026-05-28 12:08:39 +0200

    control_flow

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python3
-print("".join(c for c in "abcdefghijklmnopqrstuvwxyz" if c not in "qe"))
+print("{}".format("".join(c for c in "abcdefghijklmnopqrstuvwxyz" if c not in "qe")))
