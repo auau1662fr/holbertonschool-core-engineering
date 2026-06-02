2784a2a48494a15b1a261fb72480b511ef77d70b
auau1662fr <auric.chrn.2005@gmail.com>
2026-06-02 12:52:51 +0200

    1 3 5 6

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
-def uppercase
+
+def uppercase(str):
+    result = ""
+
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+
+    print(result)
