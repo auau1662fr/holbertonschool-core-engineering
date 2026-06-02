38682b4d6ae7c287b3d55b506e6a26b6ebaba116
auau1662fr <auric.chrn.2005@gmail.com>
2026-06-02 13:04:00 +0200

    add 3

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+
 def pow(a, b):
     result = 1
 
-    for i in range(b):
-        result *= a
+    if b >= 0:
+        for i in range(b):
+            result *= a
+    else:
+        for i in range(-b):
+            result *= a
+        result = 1 / result
 
     return result
